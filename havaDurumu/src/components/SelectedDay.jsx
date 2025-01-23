8b9462137b17c597637d3f696edcb12faca2c344
Ömer Faruk <omergny03@gmail.com>
2025-01-24 00:52:10 +0300

    weather

--- havaDurumu/src/components/SelectedDay.jsx
+++ havaDurumu/src/components/SelectedDay.jsx
@@ -1,44 +1,29 @@
 import React, { useEffect, useState } from 'react'
 import '../css/home.css'
 import '../css/animation.css'
-import Lottie from 'lottie-react';
-import gif from '../images/gunesli.gif';
+import modarateRain from '../images/moderateRain.gif';
+import partlyCloudy from '../images/partlyCloudy.gif';
 
 function SelectedDay({day}) {
-    const [animationData, setAnimationData] = useState(null);
-    const getAnimationDataUrl = () => {
-        if (day.day.condition.text === 'Cloudy') return 'https://assets10.lottiefiles.com/packages/lf20_jz5rvjiv.json';
-        if (day.day.condition.text === 'Rainy') return rainyAnimation;
+    
+  const getAnimationData = () => {
+      const conditionText = day.day.condition.text.trim().toLowerCase();
+        if (conditionText === 'moderate rain') return modarateRain;
+        if (conditionText=== 'partly cloudy') return partlyCloudy;
         return null;
       };
- 
-        useEffect(() => {
-            const fetchAnimation = async () => {
-                const url = 'https://assets2.lottiefiles.com/packages/lf20_jvxwtdtp.json';  // Yağmur animasyonu
-
-              if (url) {
-                try {
-                  const response = await fetch(url);
-                  if (!response.ok) {
-                    console.log('Animasyon yüklenemedi.')
-                  }
-                  const data = await response.json();
-                  setAnimationData(data);
-                } catch (error) {
-                  console.log('Animasyon yükleme hatası:', error);
-                }
-              }
-            };
-            fetchAnimation();
-          }, [day]);
-
+  const getAnimationColor = ()=>{
+     const conditionText = day.day.condition.text.trim().toLowerCase();
+     if (conditionText === 'moderate rain') return 'white';
+     if (conditionText=== 'partly cloudy') return 'white';
+     return null;
+  };
           return (
             <div className="weather-container">
               <div className="animation-bg">
-                {/* {animationData && <Lottie animationData={animationData} loop={true} />} */}
-                <img src={gif} alt="" style={{height:'100%',width:'100%'}}/>
+                <img src={`${getAnimationData()}`} alt="" style={{height:'100%',width:'100%'}}/>
               </div>
-              <div className="weather-info">
+              <div className="weather-info" style={{color:`${getAnimationColor()}`}}>
                 <h3>{day.date}</h3>
                 <img src={day.day.condition.icon} alt="weather-icon" />
                 <div><b>{day.day.avgtemp_c} °C</b></div>
